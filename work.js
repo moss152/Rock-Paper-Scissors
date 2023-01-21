038064f66f08fd3f005d0950478bcbaed2ada9e4
Moses <mossymos86@gmail.com>
2023-01-21 21:28:18 +0200

    Rondom function works

---
+++ work.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+
+    console.log(choices[random]);
+
+    }
+    
+
+
+
+//function rockPaperScissor(playerSelection, computerSelection){}
