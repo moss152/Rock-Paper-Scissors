977b3baa8f3b6a2a8b7ad91e6fac8828072a09a6
Moses <mossymos86@gmail.com>
2023-01-22 12:36:55 +0200

    the function works perfectly

--- work.js
+++ work.js
@@ -1,12 +1,43 @@
-function getComputerChoice(){
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(Math.random() * choices.length);
 
-    console.log(choices[random]);
 
-    }
+
+
+function rockPaperScissor(getComputerChoice){
+
+    function getComputerChoice(){
+        const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+        const random = Math.floor(Math.random() * choices.length);
+    
+        return choices[random];
+    
+        }
+    
+    const playerSelection = prompt('Rock, Paper, Scissors?').toUpperCase();
+
+    const computerSelection = getComputerChoice();
+
+        if(playerSelection === 'ROCK' && computerSelection === 'PAPER'){
+    return 'YOU LOOSE! PAPER BEATS ROCK' ;
     
 
+} else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+    return  'YOU WIN! ROCK BEATS SCISSORS';
 
+}else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+    return 'YOU WIN! PAPER BEATS ROCK';
 
-//function rockPaperScissor(playerSelection, computerSelection){}
+}else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS'){
+    return 'YOU LOOSE! SCISSORS BEATS PAPER';
+    
+}else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+    return 'YOU WIN! SCISSORS BEATS PAPER';
+
+} else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
+    return 'YOU LOOSE! ROCK BEAT SCISSORS';
+
+} else if (playerSelection === computerSelection) {
+    return 'DRAW, PLEASE PLAY AGAIN'
+}
+
+}
+console.log(rockPaperScissor());
